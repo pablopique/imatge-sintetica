13af6c13e5c517f9ced968a2071201f2f8c28add
u124268 <u124268@upf.edu>
2020-02-06 16:30:06 +0100

    lab2

---
+++ lab2/RTIS_Students_V2_2019/src/shaders/depthshader.cpp
@@ -0,0 +1,23 @@
+#include "depthshader.h"
+#include "../core/utils.h"
+
+DepthShader::DepthShader(Vector3D hitColor_, double maxDist_, Vector3D bgColor_) :
+    Shader(bgColor_), maxDist(maxDist_), color(hitColor_)
+{ }
+
+Vector3D DepthShader::computeColor(const Ray &r, const std::vector<Shape*> &objList, const std::vector<PointLightSource> &lsList) const
+{
+	Intersection closestIntersection;
+	if (Utils::getClosestIntersection(r, objList, closestIntersection)) {
+		Vector3D distance = closestIntersection.itsPoint - r.o;
+		double depthDistance = sqrt(pow(distance.x, 2) + pow(distance.y, 2) + pow(distance.z, 2));
+		if (depthDistance < maxDist) {
+			return color;
+		}
+		else {
+			return bgColor;
+		}
+	}
+
+	return bgColor;
+}
